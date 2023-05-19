4f707218dde80060bd26b9ae2059423ceddab487
sebasca9 <scastanoa1@eafit.edu.co>
2023-05-19 11:42:14 -0500

    Segunda version

--- Controladora.java
+++ Controladora.java
@@ -4,8 +4,24 @@ public class Controladora {
     private String body;
     private final Methods method = new BD();
     private int id;
+    private final BD bd = new BD();
 
-    private void procesarRequest(){
+    private void procesarRequest(String m){
+        switch (method) {
+            case "GET" -> {
+                if (cabecera.equals("http://localhost:8080/apis/users")) {
+                    GET();
+                }
+            }
+            case "POST" -> {
+                if (cabecera.equals("http://localhost:8080/apis/users")) {
+                    POST(body);
+                }
+            }
+            default -> System.out.println("Método no soportado");
+        }
+    }
+    private void procesarRequest(String m, int id){
         switch (method) {
             case "GET" -> {
                 if (cabecera.equals("http://localhost:8080/apis/users")) {
@@ -34,16 +50,34 @@ public class Controladora {
         this.tipo = tipo;
         this.cabecera = cabecera;
         this.body = body;
-
-
+        procesarRequest(body);
     }
     private void apiRest(String tipo, String cabecera, String body, int id){
         this.tipo = tipo;
         this.cabecera = cabecera;
         this.body = body;
         this.id = id;
+    }
 
-
+    public void GET(){
+        bd.mostrarTodosUsuarios();
+        System.out.println("GET");
+    }
+    public void GETUser(int id){
+        bd.mostrarUsuario(id);
+        System.out.println("GETUser");
+    }
+    public void POST(String body){
+        bd.agregarUsuario(body);
+        System.out.println("POST");
+    }
+    public void PUT(String body, int id){
+        bd.actualizarUsuario(id, body);
+        System.out.println("PUT");
+    }
+    public void DELETE(int id){
+        bd.eliminarUsuario(id);
+        System.out.println("DELETE");
     }
 }
 
